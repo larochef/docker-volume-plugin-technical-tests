49315a9829811a91b0d0ac6d0eac21c26385c474
francois_laroche <flaroche.prestataire@voyages-sncf.com>
2016-02-24 17:45:09 +0100

    Add flags to allow more configuration

--- main.go
+++ main.go
@@ -2,20 +2,27 @@ package main
 
 import (
     "os"
+    "flag"
     "path/filepath"
     "github.com/docker/go-plugins-helpers/volume"
 )
 
+const defaultPath = "/tmp/volumes"
+const pluginName = "simple-fs"
+
 func main() {
-    root := "/tmp/volumes"
+    defer os.Remove("/run/docker/" + pluginName)
+
+    root :=  flag.String("root", defaultPath, "Simple FS storage location")
+    flag.Parse()
     // Base directory to store all
-    os.MkdirAll(root, 0755)
+    os.MkdirAll(*root, 0755)
     // Metadata on volumes
-    os.Mkdir(filepath.Join(root, "definitions"), 0755)
+    os.Mkdir(filepath.Join(*root, "definitions"), 0755)
     // Data of volumes
-    os.Mkdir(filepath.Join(root, "data"), 0755)
-    os.Mkdir(filepath.Join(root, "mnt"), 0755)
-    println("Creating server from root dir", root)
-    var driver volume.Driver = newDriver(root)
-    volume.NewHandler(driver).ServeUnix("docker", "simple-fs")
+    os.Mkdir(filepath.Join(*root, "data"), 0755)
+    os.Mkdir(filepath.Join(*root, "mnt"), 0755)
+    println("Creating server from root dir", *root)
+    var driver volume.Driver = newDriver(*root)
+    volume.NewHandler(driver).ServeUnix("root", pluginName)
 }
